2079ccfc33486d399fea61319a68282718d56146
Francesc <francesc93sq@gmail.com>
2025-05-16 20:20:14 +0200

    problemas cerrar sesion

--- resources/views/components/dropdown-link.blade.php
+++ resources/views/components/dropdown-link.blade.php
@@ -1,4 +1,4 @@
-<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-3xl leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out']) }}>
+<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-3xl leading-5 text-gray-700 dark:white hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out']) }}>
     @isset($svg)
         {!! $slot !!}
     @else
