932b66f75c137ad97da785ab8db138ab5950becf
Francesc <francesc93sq@gmail.com>
2025-05-16 15:51:37 +0200

    css

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -11,7 +11,7 @@
                     <h2 class="text-bold text-5xl text-bold">{{Auth::user()->name}}</h2>
                     <fieldset>
                         <label for="text">Mensaje:</label><br>
-                        <textarea name="text" class="w-96 md:w-[600px] xl:w-[850px] 2xl:w-[1200px] text-4xl" rows="20"></textarea>
+                        <textarea name="text" class="w-96 md:w-[600px] xl:w-[850px] 2xl:w-[1200px] text-4xl"></textarea>
                     </fieldset>
                     <input class="buttonBlue" type="submit" value="Enviar">
                 </form>
