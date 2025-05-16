00a35ca0fd25a8e837ac2da14bb0782a2e31e83f
Francesc <francesc93sq@gmail.com>
2025-05-16 19:35:03 +0200

    css

--- resources/views/components/cards-div.blade.php
+++ resources/views/components/cards-div.blade.php
@@ -1,3 +1,3 @@
-<section class="w-full p-2 gap-2 flex flex-col items-center md:grid @isset($images) overflow-x-scroll md:grid-cols-4 @else md:grid-cols-2  @endisset">
+<section class="w-full p-2 gap-2 flex flex-col items-center lg:grid @isset($images) overflow-x-scroll md:grid-cols-4 @else md:grid-cols-2  @endisset">
    {{$slot}}
 </section>
