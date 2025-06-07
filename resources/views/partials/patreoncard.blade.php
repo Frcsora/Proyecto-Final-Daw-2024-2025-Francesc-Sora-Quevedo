003deec8c6bc0aa8ca52ddcc70f11c2e66c1603e
Francesc <francesc93sq@gmail.com>
2025-06-07 14:56:27 +0200

    cambios finales css

--- resources/views/partials/patreoncard.blade.php
+++ resources/views/partials/patreoncard.blade.php
@@ -1,10 +1,10 @@
 <section class="flex flex-col">
     <x-card>
+        <x-slot:show></x-slot:show>
         <section>
             <x-slot:patreon></x-slot:patreon>
             <h1 class="text-2xl lg:text-3xl">Unete a nosotros en <a href="https://www.patreon.com/PioPioeSports">patreon</a></h1>
             <p class="text-lg lg:text-2xl">¡Suscríbete en patreon para formar parte de algo más grande y apoyar el futuro de los eSports!</p>
-
         </section>
 
     </x-card>
