32e44f7a086587c8ed3fd9dbce775026803ec56e
Francesc <francesc93sq@gmail.com>
2025-06-07 19:49:52 +0200

    cambios finales css

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -5,7 +5,7 @@
             <x-slot:show></x-slot:show>
             <h2 class="text-xl md:text-2xl lg:text-4xl">{{$newsvar->title}}</h2>
             <small>Creado por {{$newsvar->user->name}} el {{$newsvar->created_at}}</small><br>
-            <img class="lg:w-96 rounded-lg max-h-96" src="{{$newsvar->image}}" alt="imagen noticia">
+            <img class="w-[100%] rounded-lg " src="{{$newsvar->image}}" alt="imagen noticia">
             <p class="bg-white p-1">{{$newsvar->text}}</p>
             @if(count($newsvar->tags) > 0)
                 <p>
