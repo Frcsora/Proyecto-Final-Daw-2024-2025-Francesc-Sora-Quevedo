f249a6fe8359139a83b4a56f6665cae6d752672d
Francesc <francesc93sq@gmail.com>
2025-06-07 18:08:16 +0200

    cambios finales css

--- resources/views/components/cards-div.blade.php
+++ resources/views/components/cards-div.blade.php
@@ -1,4 +1,4 @@
-<section class="w-fullp-2 gap-2 grid grid-cols-1 justify-items-center @isset($images) overflow-x-scroll md:grid-cols-4 @endisset">
+<section class="w-full p-2 gap-2 grid grid-cols-1 justify-items-center @isset($images) overflow-x-scroll md:grid-cols-4 @endisset">
    {{$slot}}
 </section>
 
