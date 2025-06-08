5793a19a18ee77926db3594856c0e5c001d07644
Francesc <francesc93sq@gmail.com>
2025-06-08 14:19:33 +0200

    cambios finales css

--- resources/views/partials/patreoncard.blade.php
+++ resources/views/partials/patreoncard.blade.php
@@ -1,1 +1,1 @@
-<a href="https://www.patreon.com/PioPioeSports" target="_blank" rel="noopener noreferrer"><img src="{{asset('images/patreon.png')}}" alt="Banner patreon" class="w-full rounded-lg"></a>
+<a href="https://www.patreon.com/PioPioeSports" target="_blank" rel="noopener noreferrer"><img src="{{asset('images/patreon.png')}}" alt="Banner patreon" class="w-64 md:w-96 lg:w-full rounded-lg"></a>
