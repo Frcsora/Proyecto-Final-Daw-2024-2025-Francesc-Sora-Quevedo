cfde18fee54303fb7d95405a7e557bd1fda448cf
Francesc <francesc93sq@gmail.com>
2025-06-08 15:52:35 +0200

    cambios finales css

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -5,7 +5,7 @@
             <x-card>
                 <x-slot:show></x-slot:show>
                 @include('partials.errors')
-                <form class="h-[70vw]" id="formMessage" action="{{route('message')}}" method="POST">
+                <form class="h-[100%]" id="formMessage" action="{{route('message')}}" method="POST">
                     @csrf
                     <input name="email" type="hidden" value="{{Auth::user()->email}}">
                     <input type="hidden" name="name" value="{{Auth::user()->name}}">
