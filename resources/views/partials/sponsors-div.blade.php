97964e723c2edcbe57fa857902195e1a645615f1
Francesc <francesc93sq@gmail.com>
2025-06-08 15:13:50 +0200

    cambios finales css

--- resources/views/partials/sponsors-div.blade.php
+++ resources/views/partials/sponsors-div.blade.php
@@ -1,12 +1,11 @@
 @if(count($sponsors) > 0)
     <x-card>
-        <x-slot:show>
+        <x-slot:show></x-slot:show>
             <h2>Patrocinadores</h2>
             <section class="flex flex-wrap gap-2">
                 @foreach($sponsors as $sponsor)
                     <a href="{{$sponsor->link}}" target="_blank" rel="noopener noreferrer"><img class="w-24" alt="{{$sponsor->name}}" src="{{$sponsor->base64}}"></a>
                 @endforeach
             </section>
-        </x-slot:show>
     </x-card>
 @endif
