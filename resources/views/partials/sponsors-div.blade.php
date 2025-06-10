30fa8dc977bac3033361a81f5796f32408fe264d
Francesc <francesc93sq@gmail.com>
2025-06-10 12:41:09 +0200

    cambios finales css

--- resources/views/partials/sponsors-div.blade.php
+++ resources/views/partials/sponsors-div.blade.php
@@ -4,7 +4,7 @@
             <h2>Patrocinadores</h2>
             <section class="flex flex-wrap gap-2">
                 @foreach($sponsors as $sponsor)
-                    <a href="{{$sponsor->link}}" target="_blank" rel="noopener noreferrer"><img class="w-24" alt="{{$sponsor->name}}" src="{{$sponsor->base64}}"></a>
+                    <a href="{{$sponsor->link}}" target="_blank" rel="noopener noreferrer"><img class="w-24 h-16" alt="{{$sponsor->name}}" src="{{$sponsor->base64}}"></a>
                 @endforeach
             </section>
     </x-card>
